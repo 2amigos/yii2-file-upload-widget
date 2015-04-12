97167262625c0d0c7d219e570be467362805773a
Alexander Kochetov <creocoder@gmail.com>
2015-04-12 23:45:45 +0300

    Replaced source files headers

--- src/FileUploadPlusAsset.php
+++ src/FileUploadPlusAsset.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @link https://github.com/2amigos/yii2-selectize-widget
+ * @link https://github.com/2amigos/yii2-file-upload-widget
  * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
  * @license http://opensource.org/licenses/BSD-3-Clause
  */
