f2e78fc521851986416b1ed3e755c148e9e46133
Alexander Kochetov <creocoder@gmail.com>
2015-04-12 23:38:37 +0300

    Updated dosamigos\fileupload\FileUploadPlusAsset

--- src/FileUploadPlusAsset.php
+++ src/FileUploadPlusAsset.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
- * @link http://2amigos.us
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link https://github.com/2amigos/yii2-selectize-widget
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
+
 namespace dosamigos\fileupload;
 
 use yii\web\AssetBundle;
@@ -11,19 +12,14 @@ use yii\web\AssetBundle;
 /**
  * FileUploadPlusAsset
  *
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
- * @link http://www.2amigos.us/
- * @package dosamigos\fileupload
+ * @author Alexander Kochetov <creocoder@gmail.com>
  */
 class FileUploadPlusAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/2amigos/yii2-file-upload-widget/assets/';
-
+    public $sourcePath = '@bower';
     public $css = [
         'blueimp-file-upload/css/jquery.fileupload.css'
     ];
-
     public $js = [
         'blueimp-load-image/js/load-image.min.js',
         'blueimp-canvas-to-blob/js/canvas-to-blob.min.js',
@@ -34,8 +30,7 @@ class FileUploadPlusAsset extends AssetBundle
         'blueimp-file-upload/js/jquery.fileupload-video.js',
         'blueimp-file-upload/js/jquery.fileupload-validate.js'
     ];
-
     public $depends = [
         'dosamigos\fileupload\FileUploadAsset',
     ];
-} 
+}
