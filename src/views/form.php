b3710fe67f95d766032afc72f98bffa262369421
Ivan Pushkin <imetalguardi@gmail.com>
2015-05-08 18:10:24 +0300

    fix template id and change form to div

--- src/views/form.php
+++ src/views/form.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 $context = $this->context;
 ?>
     <!-- The file upload form used as target for the file upload widget -->
-<?= Html::beginForm($context->url, 'post', $context->options); ?>
+<?= Html::beginTag('div', $context->options); ?>
     <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
     <div class="row fileupload-buttonbar">
         <div class="col-lg-7">
@@ -47,4 +47,4 @@ $context = $this->context;
     </div>
     <!-- The table listing the files available for upload/download -->
     <table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
-<?= Html::endForm();?>
+<?= Html::endTag('div');?>
