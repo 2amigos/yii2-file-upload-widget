2423bc5055cd7fd5f814a410bf3cfe4145ecf4ec
Erkan Karataş <karataserkan@users.noreply.github.com>
2015-07-29 14:26:23 +0300

    Create fileupload.php

---
+++ src/messages/tr-TR/fileupload.php
@@ -0,0 +1,10 @@
+return [
+    'Add files' => 'Dosyaları Ekle',
+    'Start upload' => 'Yüklemeye Başla',
+    'Cancel upload' => 'Yüklemeyi İptal Et',
+    'Start' => 'Başla',
+    'Cancel' => 'İptal',
+    'Error' => 'Hata',
+    'Delete' => 'Sil',
+    'Processing' => 'İşleniyor',
+];
