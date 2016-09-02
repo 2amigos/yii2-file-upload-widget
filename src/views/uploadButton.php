241971cfa61490e07abe3de791357776049f8b67
sztyvny <sztyvny@10g.pl>
2016-09-02 11:13:46 +0200

    Update uploadButton.php
    All translations in widget should be in the same category.

--- src/views/uploadButton.php
+++ src/views/uploadButton.php
@@ -5,7 +5,7 @@
 
 <span class="btn btn-success fileinput-button">
    <i class="glyphicon glyphicon-plus"></i>
-   <span><?= Yii::t('app', 'Select file...') ?></span>
+   <span><?= Yii::t('fileupload', 'Select file...') ?></span>
    <!-- The file input field used as target for the file upload widget -->
     <?= $input ?>
 </span>
