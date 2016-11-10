4ae8bf7bcba071bd88585cb85beee35c12eb25f2
Aigor79 <aigor@eldatech.it>
2016-11-10 13:25:36 +0100

    Create fileupload.php

---
+++ src/messages/it/fileupload.php
@@ -0,0 +1,11 @@
+return [
+  'Add files' => 'Aggiungi file',
+  'Start upload' => 'Inizia Upload',
+  'Cancel upload' => 'Annulla upload',
+  'Start' => 'Inizia',
+  'Cancel' => 'Annulla',
+  'Error' => 'Errore',
+  'Delete' => 'Cancella',
+  'Processing' => 'In elaborazione',
+  'Select file...' => 'Seleziona file'
+];
