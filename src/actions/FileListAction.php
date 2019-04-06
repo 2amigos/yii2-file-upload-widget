abe37a72ab20b6e90b3dc9cbd735d99685d07272
tonydspaniard <antonio@2amigos.us>
2019-04-06 19:15:25 +0200

    update php-doc

--- src/actions/FileListAction.php
+++ src/actions/FileListAction.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link https://github.com/2amigos/yii2-file-upload-widget
- * @copyright Copyright (c) 2013-2017 2amigOS! Consulting Group LLC
+ * @copyright Copyright (c) 2013-2019 2amigOS! Consulting Group LLC
  * @license http://opensource.org/licenses/BSD-3-Clause
  */
 namespace dosamigos\fileupload\actions;
